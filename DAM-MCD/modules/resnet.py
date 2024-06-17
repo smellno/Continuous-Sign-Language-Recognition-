85f5b2610a2d4f373cc0be04910d1883f1e02138
smellno <138204142+smellno@users.noreply.github.com>
2024-06-17 22:59:00 +0800

    Update resnet.py

--- DAM-MCD/modules/resnet.py
+++ DAM-MCD/modules/resnet.py
@@ -15,36 +15,6 @@ model_urls = {
 }
 
 
-class Get_Correlation(nn.Module):
-    def __init__(self, channels):
-        super().__init__()
-        reduction_channel = channels//16
-        self.down_conv = nn.Conv3d(channels, reduction_channel, kernel_size=1, bias=False)
-
-        self.down_conv2 = nn.Conv3d(channels, channels, kernel_size=1, bias=False)
-        self.spatial_aggregation1 = nn.Conv3d(reduction_channel, reduction_channel, kernel_size=(9,3,3), padding=(4,1,1), groups=reduction_channel)
-        self.spatial_aggregation2 = nn.Conv3d(reduction_channel, reduction_channel, kernel_size=(9,3,3), padding=(4,2,2), dilation=(1,2,2), groups=reduction_channel)
-        self.spatial_aggregation3 = nn.Conv3d(reduction_channel, reduction_channel, kernel_size=(9,3,3), padding=(4,3,3), dilation=(1,3,3), groups=reduction_channel)
-        self.weights = nn.Parameter(torch.ones(3) / 3, requires_grad=True)
-        self.weights2 = nn.Parameter(torch.ones(2) / 2, requires_grad=True)
-        self.conv_back = nn.Conv3d(reduction_channel, channels, kernel_size=1, bias=False)
-
-    def forward(self, x):
-
-        x2 = self.down_conv2(x)
-        affinities = torch.einsum('bcthw,bctsd->bthwsd', x, torch.concat([x2[:,:,1:], x2[:,:,-1:]], 2))
-        affinities2 = torch.einsum('bcthw,bctsd->bthwsd', x, torch.concat([x2[:,:,:1], x2[:,:,:-1]], 2))
-        features = torch.einsum('bctsd,bthwsd->bcthw', torch.concat([x2[:,:,1:], x2[:,:,-1:]], 2), F.sigmoid(affinities)-0.5 )* self.weights2[0] + \
-            torch.einsum('bctsd,bthwsd->bcthw', torch.concat([x2[:,:,:1], x2[:,:,:-1]], 2), F.sigmoid(affinities2)-0.5 ) * self.weights2[1] 
-
-        x = self.down_conv(x)
-        aggregated_x = self.spatial_aggregation1(x)*self.weights[0] + self.spatial_aggregation2(x)*self.weights[1] \
-                    + self.spatial_aggregation3(x)*self.weights[2]
-        aggregated_x = self.conv_back(aggregated_x)
-
-        return features * (F.sigmoid(aggregated_x)-0.5)
-
-
 def conv3x3(in_planes, out_planes, stride=1):
     # 3x3x3 convolution with padding
     return nn.Conv3d(
@@ -99,12 +69,9 @@ class ResNet(nn.Module):
         self.maxpool = nn.MaxPool3d(kernel_size=(1,3,3), stride=(1,2,2), padding=(0,1,1))
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.corr1 = Get_Correlation(self.inplanes)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.corr2 = Get_Correlation(self.inplanes)
         self.alpha = nn.Parameter(torch.zeros(3), requires_grad=True)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.corr3 = Get_Correlation(self.inplanes)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
         
@@ -142,11 +109,8 @@ class ResNet(nn.Module):
 
         x = self.layer1(x)
         x = self.layer2(x) 
-        x = x + self.corr1(x) * self.alpha[0]
         x = self.layer3(x)
-        x = x + self.corr2(x) * self.alpha[1]
         x = self.layer4(x)
-        x = x + self.corr3(x) * self.alpha[2]
         x = x.transpose(1,2).contiguous()
         x = x.view((-1,)+x.size()[2:]) #bt,c,h,w
 
